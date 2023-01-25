e10f0aa2cf5ce1cd580558c464a2f0106e36ffde
TheJoshuaTerry <joshterry1990@live.com>
2023-01-25 10:15:52 -0800

    added space

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,11 +11,11 @@ int main(void)
 	long int c;
 	long long int d;
 	float e;
+
 	printf("Size of a char: %lu byte(s)\n", (int)sizeof(a));
 	printf("Size of a int: %lu byte(s)\n", (int)sizeof(b));
 	printf("Size of a long int: %lu byte(s)\n", (int)sizeof(c));
 	printf("Size of a long long int: %lu byte(s)\n", (int)sizeof(d));
 	printf("Size of a float: %lu byte(s)\n", (int)sizeof(e));
-	
 	return (0);
 }

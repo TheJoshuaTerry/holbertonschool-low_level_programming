93ad5d49fea080e4d3dfb8e5330b33ad0a29c835
TheJoshuaTerry <joshterry1990@live.com>
2023-01-25 09:23:29 -0800

    Betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
 /**
- * File: 4-puts.c
- *
- * coder: Joshua Terry
- */
-int main(void)
-/**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);

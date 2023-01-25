bdf01b3041040037193eb37044d9fcdf53c33592
TheJoshuaTerry <joshterry1990@live.com>
2023-01-25 15:48:15 -0800

    alphebet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch < 123; ch++)
+	{
+		putchar(ch);
+	}
+	putchar(10);
+	return (0);
+}

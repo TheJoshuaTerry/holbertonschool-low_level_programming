00604b48baa9b129a0e6bc024cbfef99bca60b01
TheJoshuaTerry <joshterry1990@live.com>
2023-01-26 12:11:03 -0800

    print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_last_digit - see description
+ * @n: number
+ * Description: Print alpha 10 times
+ * Return: Always 0 (Success)
+*/
+int print_last_digit(int n)
+{
+	int x = n % 10;
+
+	return (x);
+}

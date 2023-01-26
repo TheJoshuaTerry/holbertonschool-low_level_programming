e05e3e161d9ab3b783c5e6d2baaa5fdb9784aa31
TheJoshuaTerry <joshterry1990@live.com>
2023-01-26 11:29:36 -0800

    is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _islower - see description
+ * Description: Print alpha 10 times
+ * Return: Always 0 (Success)
+*/
+int _islower(int c) 
+{
+	if (c > 96)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,6 +2,7 @@
 #define HEADER_H
 
 int _putchar(char c);
+int _islower(int c);
 void print_alphabet_x10(void);
 void print_alphabet(void);
 #endif

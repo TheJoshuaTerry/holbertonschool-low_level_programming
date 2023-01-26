f574b7b61b7b771b2062c79e55640faa2a261cbc
TheJoshuaTerry <joshterry1990@live.com>
2023-01-26 08:05:13 -0800

    print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar(10);
+	return (0);
+}
